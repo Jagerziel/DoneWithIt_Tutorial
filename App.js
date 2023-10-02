553f43f190eeeaa15549c55ce2ef7c1c46619bac
Jagerziel <fromnytosg@gmail.com>
2023-10-02 14:22:12 -0400

    Exploring touchable components

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Platform, Image, StatusBar, TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
-  console.log(`App Executed`);
-
-  let x = 1;
+  function handlePress () {console.log('text pressed')} 
 
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text 
+        // numberOfLines={1} 
+        onPress={handlePress}
+        selectable={false}
+      >
+        React Native
+      </Text>
+      {/* <Image source={require('./assets/icon.png')} style={{height: 100, aspectRatio: 1}}/> */}
+      <TouchableWithoutFeedback onPress={() => console.log("image tapped")}>
+        <Image 
+          source={{uri: "https://picsum.photos/200/300", width: 200, height: 300}}
+          blurRadius={1}
+          fadeDuration={3000} // Android Only
+        />
+      </TouchableWithoutFeedback>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -18,6 +31,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     alignItems: "center",
     justifyContent: "center",
   },
