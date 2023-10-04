675bc50ce01e92d11277387232b3dd1385c64cae
Jagerziel <fromnytosg@gmail.com>
2023-10-04 13:17:00 -0400

    moving assets folder contents

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 // import { StatusBar } from "expo-status-bar";
 import { StyleSheet,  View, SafeAreaView , Platform, StatusBar} from "react-native";
 
-import { Playground } from "./Playground.js";
+// import { Playground } from "./Playground.js";
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Playground />
+
     </SafeAreaView>
 
   );
@@ -18,8 +18,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: "center",
-    // justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
   
 });
